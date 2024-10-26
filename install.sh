264c84f47e7453a61fe9f147f59c49182c7117c1
amogus-creator <t84214583@gmail.com>
2024-10-26 15:30:00 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo "Установка необходимых зависимостей... ( xorg-server, bspwm, alacritty, sxhkd, vim, ly)"
-sudo pacman -S --noconfirm xorg-server bspwm alacritty sxhkd vim ly
+echo "Установка необходимых зависимостей... ( xorg-server, bspwm, alacritty, sxhkd, vim, ly, picom, zsh, feh)"
+sudo pacman -S --noconfirm xorg-server bspwm alacritty sxhkd vim ly picom zsh feh
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 systemctl enable ly.service
 
 echo "Создание директорий для конфигурации... ( ~/.config/bspwm )"
 mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/picom
 
 echo "Копирование конфигурационных файлов..."
 mv ~/Downloads/bspwm-example/bspwmrc ~/.config/bspwm/
 mv ~/Downloads/bspwm-example/sxhkdrc ~/.config/bspwm/
 mv ~/Downloads/bspwm-example/alacritty.toml ~/.config/alacritty/
+mv ~/Downloads/bspwm-example/picom.conf ~/.config/picom/
 mv ~/Downloads/bspwm-example/.xsession ~/
 
 chmod +x ~/.config/bspwm/bspwmrc
