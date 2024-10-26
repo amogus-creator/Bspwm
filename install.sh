bbf302a9003111aba93420c85178d77c2743658d
amogus-creator <t84214583@gmail.com>
2024-10-26 15:19:46 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ systemctl enable ly.service
 
 echo "Создание директорий для конфигурации... ( ~/.config/bspwm )"
 mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/alacritty
 
 echo "Копирование конфигурационных файлов..."
 mv ~/Downloads/bspwm-example/bspwmrc ~/.config/bspwm/
 mv ~/Downloads/bspwm-example/sxhkdrc ~/.config/bspwm/
+mv ~/Downloads/bspwm-example/alacritty.toml ~/.config/alacritty/
 mv ~/Downloads/bspwm-example/.xsession ~/
 
 chmod +x ~/.config/bspwm/bspwmrc
