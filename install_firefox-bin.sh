fc05197247d1e235317d583fb4449183b66a5c60
amogus-creator <t84214583@gmail.com>
2024-10-27 17:45:45 +0300

    Update install_firefox-bin.sh

--- install_firefox-bin.sh
+++ install_firefox-bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Установка необходимых зависимостей... (curl, tar, alsa-lib)"
-sudo pacman -S --noconfirm curl tar alsa-lib
+echo "Установка необходимых зависимостей... (curl, tar, alsa-lib, gtk3)"
+sudo pacman -S --noconfirm curl tar alsa-lib gtk3
 
 cd ~/Downloads || { echo "Не удалось перейти в папку загрузок. ~/Downloads"; exit 1; }
 
