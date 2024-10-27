e1ac5713c5ac9ea14ad6857baa90bbe0010eb530
amogus-creator <t84214583@gmail.com>
2024-10-27 21:48:44 +0300

    Update cursor.sh

--- cursor.sh
+++ cursor.sh
@@ -6,7 +6,18 @@ tar xvf Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
 
 tar xvf Nordzy-cursors-white.tar.gz -C /usr/share/icons
 
-echo 
+mkdir ~/.config/gtk-3.0
 
+touch ~/.config/gtk-3.0/settings.ini
 
+echo '[Settings] gtk-cursor-theme-name=Nordzy-cursors-white' >> ~/.config/gtk-3.0/settings.ini
 
+touch ~/.gtkrc-2.0
+
+echo 'gtk-cursor-theme-name="Nordzy-cursors-white"' >> ~/.gtkrc-2.0
+
+xrdb ~/.Xresources
+
+echo '' >> ~/.config/bspwm/bspwmrc
+
+echo 'xsetroot -cursor_name left_ptr' >> ~/.config/bspwm/bspwmrc
