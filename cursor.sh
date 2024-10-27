e75bb4fe8b4339c3ac7bb9e49e7bc24a22e9215f
amogus-creator <t84214583@gmail.com>
2024-10-27 21:39:43 +0300

    Update cursor.sh

--- cursor.sh
+++ cursor.sh
@@ -1,22 +1,12 @@
-#!/bin/bash
+sudo pacman -S --noconfirm gkt2 gtk3
 
-# Путь к архиву с курсорами
-CURSOR_ARCHIVE=~/Downloads/Nordzy-cursors-white.tar.gz
+mkdir -p ~/.local/share/icons
 
-# Распаковка архива в директорию для курсоров
-mkdir -p ~/.icons/Nordzy-cursors-white
-tar -xzf "$CURSOR_ARCHIVE" -C ~/.icons/Nordzy-cursors-white
+tar xvf Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
 
-# Настройка GTK и Xresources
-mkdir -p ~/.config/gtk-3.0
-echo 'gtk-cursor-theme-name="Nordzy-cursors-white"' > ~/.config/gtk-3.0/settings.ini
-echo 'XCursor.theme: Nordzy-cursors-white' >> ~/.Xresources
+tar xvf Nordzy-cursors-white.tar.gz -C /usr/share/icons
 
-# Применение изменений
-xrdb ~/.Xresources
-xsetroot -cursor_name left_ptr
+echo 
 
-# Настройка bspwmrc
-echo 'xsetroot -cursor_name left_ptr' >> ~/.config/bspwm/bspwmrc
 
-echo "Курсор установлен и настроен. Пожалуйста, перезапустите сеанс или систему для применения изменений."
+
