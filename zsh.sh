1e8462bcc716dea7cfc857c9a613050846b217f3
amogus-creator <t84214583@gmail.com>
2024-10-27 18:05:05 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,4 +6,11 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
 chsh -s $(which zsh)
+
+echo "omg theme list"
+echo "установлена тема powerlevel10k"
+
+
